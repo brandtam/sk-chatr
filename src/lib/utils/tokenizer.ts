78928da50a2809123bb2e2b1f25aec5217fafe00
Brandt Milczewski <brandt@shopdevs.com>
2024-03-21 11:33:38 -0700

    Remove unused await

--- src/lib/utils/tokenizer.ts
+++ src/lib/utils/tokenizer.ts
@@ -1,8 +1,8 @@
 import { encodingForModel } from "js-tiktoken";
 
-const encoder = await encodingForModel("gpt-4");
+const encoder = encodingForModel("gpt-4");
 
-export function getTokens(input: string): number {
+export async function getTokens(input: string): number {
 	const tokens = encoder.encode(input);
 	return tokens.length;
 }
